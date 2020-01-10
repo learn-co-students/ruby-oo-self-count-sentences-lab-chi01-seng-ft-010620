9bc92ff98e44b897847c292a9adcbe67155ac3ac
Lindsay Mecher <lindsaymecher@gmail.com>
2020-01-10 13:05:18 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,32 @@
 require 'pry'
 
 class String
+    attr_accessor :count
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
-
+  
+  def count_sentences      
+    sentence_array = self.split(/\.|\?|\!/).delete_if do |sentence|
+      sentence.length < 2
+    end
+    sentence_array.length
   end
+
 end
+# .split(/\,|\!|\@|\$/)
+
+# / /
+
+# split bookends are //
+# backslash before each new thing
